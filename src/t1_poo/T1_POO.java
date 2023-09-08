8e27a1867f1bad82dd69762a4b914254b225bea0
LeninPaytanSarmiento07 <142622554+LeninPaytanSarmiento07@users.noreply.github.com>
2023-09-08 13:57:12 -0500

    Add files via upload

---
+++ src/t1_poo/T1_POO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1_poo;
+
+/**
+ *
+ * @author Lenin
+ */
+public class T1_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PersonaLista per = new PersonaLista();
+        per.setLocationRelativeTo(null);
+        per.setVisible(true);
+    }
+    
+}
